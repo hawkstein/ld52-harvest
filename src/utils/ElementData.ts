8601d745b996615d5c3cfea10c2214d888fa003b
hawkstein <liamhawks+liamhawks@gmail.com>
2023-01-07 16:56:04 +0000

    start adding more items

--- src/utils/ElementData.ts
+++ src/utils/ElementData.ts
@@ -8,4 +8,6 @@ export const Options: ElementOption[] = [
   { name: "Small Pumpkin", colour: 0xf5a825, size: 1 },
   { name: "Large Pumpkin", colour: 0xf58c25, size: 2 },
   { name: "Giant Pumpkin", colour: 0xc95019, size: 3 },
+  { name: "Red Apple", colour: 0xde330d, size: 1 },
+  { name: "Green Apple", colour: 0x3ea308, size: 1 },
 ];
