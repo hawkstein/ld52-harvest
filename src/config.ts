76b61bd764f7bbea1205d1ddbff2fbef36a6ba10
hawkstein <liamhawks+liamhawks@gmail.com>
2023-01-07 10:12:38 +0000

    [feat] setup game config for mobile

--- src/config.ts
+++ src/config.ts
@@ -3,10 +3,10 @@ import Phaser from "phaser";
 export default {
   type: Phaser.AUTO,
   parent: "game",
-  backgroundColor: "#000",
+  backgroundColor: "#f5e5b8",
   scale: {
-    width: 960,
-    height: 540,
+    width: 640,
+    height: 340,
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
   },
