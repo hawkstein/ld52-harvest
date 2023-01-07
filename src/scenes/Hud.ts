ecdb737f900558abf11a9c0664569a7f6a900f46
hawkstein <liamhawks+liamhawks@gmail.com>
2023-01-07 11:11:09 +0000

    bring selection ui down from the top

--- src/scenes/Hud.ts
+++ src/scenes/Hud.ts
@@ -21,14 +21,14 @@ export default class HUD extends Phaser.Scene {
     this.background = this.add.graphics();
     this.background.fillStyle(0x222222, 0.8);
     this.background.fillRect(0, 0, boxWidth, boxHeight);
-    this.background.setPosition(-boxWidth, 0);
+    this.background.setPosition(0, -boxHeight);
 
-    const button = this.add.rectangle(0, 0, 30, 30, 0xff2222).setOrigin(0, 0);
+    const button = this.add.rectangle(30, 0, 60, 50, 0xff2222).setOrigin(0, 0);
     button.setInteractive();
     button.on("pointerdown", () => {
       this.tweens.add({
         targets: this.background,
-        x: this.expanded ? -boxWidth : 0,
+        y: this.expanded ? -boxHeight : 0,
         ease: "Power1",
         duration: 300,
       });
