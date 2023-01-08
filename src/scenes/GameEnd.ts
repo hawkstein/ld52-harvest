ce84f3ed81f9834d32840abfb7b47f7b9908c4f5
hawkstein <liamhawks+liamhawks@gmail.com>
2023-01-08 12:54:37 +0000

    update GAME_END msg

--- src/scenes/GameEnd.ts
+++ src/scenes/GameEnd.ts
@@ -1,5 +1,6 @@
 import Phaser from "phaser";
 import Scenes from "@scenes";
+import { FONT_FAM, TXT_COLOR } from "config";
 
 export default class GameEnd extends Phaser.Scene {
   constructor() {
@@ -7,13 +8,13 @@ export default class GameEnd extends Phaser.Scene {
   }
 
   create() {
-    const congrats = this.add.text(
-      this.cameras.main.centerX,
-      this.cameras.main.centerY,
-      "Well done! You beat the game!",
-      { color: "#fff" }
-    );
-    congrats.x -= congrats.width / 2;
-    congrats.y -= congrats.height / 2;
+    this.add
+      .text(
+        this.cameras.main.centerX,
+        this.cameras.main.centerY,
+        "That's it! I've had enough!\n\nIt's been 10 years, I'm never going to win!",
+        { fontFamily: FONT_FAM, color: TXT_COLOR }
+      )
+      .setOrigin(0);
   }
 }
