4cae07e047a2f1c409747e706e7bee556e8e86d9
hawkstein <liamhawks+liamhawks@gmail.com>
2023-01-08 20:08:38 +0000

    always display judges

--- src/scenes/YearIntro.ts
+++ src/scenes/YearIntro.ts
@@ -21,17 +21,15 @@ export default class YearIntro extends Phaser.Scene {
       )
       .setOrigin(0.5);
 
-    if (year === 1) {
-      const judges = progress.getJudges().map(({ name }) => name);
-      this.add
-        .text(
-          this.cameras.main.centerX,
-          this.cameras.main.centerY - 50,
-          `Your judges are ${judges[0]}, ${judges[1]} and ${judges[2]}.`,
-          { color: "#000", fontSize: "24px", fontFamily: FONT_FAM }
-        )
-        .setOrigin(0.5);
-    }
+    const judges = progress.getJudges().map(({ name }) => name);
+    this.add
+      .text(
+        this.cameras.main.centerX,
+        this.cameras.main.centerY - 50,
+        `Your judges are ${judges[0]}, ${judges[1]} and ${judges[2]}.`,
+        { color: "#000", fontSize: "24px", fontFamily: FONT_FAM }
+      )
+      .setOrigin(0.5);
 
     const startButton = new GameButton({
       x: this.cameras.main.centerX - 60,
