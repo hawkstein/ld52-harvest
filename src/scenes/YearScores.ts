07887fa7e9e23f59e055476b1a9cb1d2b925c35d
hawkstein <liamhawks+liamhawks@gmail.com>
2023-01-08 13:51:45 +0000

    add score message

--- src/scenes/YearScores.ts
+++ src/scenes/YearScores.ts
@@ -3,6 +3,39 @@ import Scenes from "@scenes";
 import { FADE_LENGTH, FONT_FAM, FONT_SIZE, TXT_COLOR } from "config";
 import { ScoreCard } from "sprites/ScoreCard";
 
+const averageScore = (scores: Record<string, number>) =>
+  Math.round(
+    Object.values(scores).reduce((total, score) => total + score, 0) / 3
+  );
+
+const getPlacementMessage = (score: number) => {
+  switch (score) {
+    case 0:
+      return "You came last. Out of 44 starred eyed hopefuls.";
+    case 1:
+      return "Bottom of the barrel. 36th place is yours.";
+    case 2:
+      return "You came 25th. Best keep this quiet or Brenda will talk.";
+    case 3:
+      return "You came 20th. Hopefully you can improve.";
+    case 4:
+      return "12th place is yours. Onwards and upwards!";
+    case 5:
+      return "7th place. Did you come here to lose? or to win!";
+    case 6:
+      return "5th place is yours. You can do better.";
+    case 7:
+      return "4th place. Some of these other displays are good!";
+    case 8:
+      return "3rd place. Nearly there!";
+    case 9:
+      return "So close! 2nd place in the competition. Next year!";
+    case 10:
+      return "You won! You came first! You have beaten all others!";
+    default:
+      return "You came last. For cheating.";
+  }
+};
 export default class YearScores extends Phaser.Scene {
   constructor() {
     super(Scenes.YEAR_SCORES);
@@ -30,10 +63,13 @@ export default class YearScores extends Phaser.Scene {
       });
     });
 
+    const average = averageScore(scores);
+    const placementMessage = getPlacementMessage(average);
+
     const place = this.add
-      .text(this.cameras.main.centerX, 280, "You came in 4th place", {
+      .text(this.cameras.main.centerX, 280, placementMessage, {
         color: TXT_COLOR,
-        fontSize: "36px",
+        fontSize: "28px",
         fontFamily: FONT_FAM,
       })
       .setOrigin(0.5)
